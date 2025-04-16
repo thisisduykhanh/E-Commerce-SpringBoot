4018efc2ba0e4cfed391281f7f4f12e224a17bbf
Duy Khanh <162108959+this-is-duykhanh@users.noreply.github.com>
2025-04-16 13:02:55 +0700

    check observer pattern

--- e-commerce-api/src/main/java/com/example/e_commerce_api/pattern/observer/CartUpdateSubject.java
+++ e-commerce-api/src/main/java/com/example/e_commerce_api/pattern/observer/CartUpdateSubject.java
@@ -1,18 +1,11 @@
 package com.example.e_commerce_api.pattern.observer;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CartUpdateSubject extends CartSubject {
 
-    @Autowired
-    private SimpMessagingTemplate messagingTemplate;
-
     @Override
     public void notifyCartUpdated(Integer accountId) {
-        String destination = "/topic/cart/" + accountId;
-        messagingTemplate.convertAndSend(destination, "Cart updated");
+        super.notifyCartUpdated(accountId);
     }
 }
--- e-commerce-api/src/main/java/com/example/e_commerce_api/pattern/observer/CartWebSocketNotifier.java
+++ e-commerce-api/src/main/java/com/example/e_commerce_api/pattern/observer/CartWebSocketNotifier.java
@@ -1,16 +1,18 @@
 package com.example.e_commerce_api.pattern.observer;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CartWebSocketNotifier implements CartObserver {
 
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
     // inject WebSocket service, or use messagingTemplate if with STOMP
     @Override
     public void updateCart(Integer accountId) {
-        // Notify the WebSocket clients about the cart update
-        // For example, you can use SimpMessagingTemplate to send a message to a specific topic
-        // messagingTemplate.convertAndSend("/topic/cart/" + accountId, "Cart updated for account ID: " + accountId);
-        System.out.println("WebSocket notification: Cart updated for account ID: " + accountId);
+        String destination = "/topic/cart/" + accountId;
+        messagingTemplate.convertAndSend(destination, "Cart updated");
     }
 }
